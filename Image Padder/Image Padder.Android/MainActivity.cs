5737a712137060a00d5132b736f2c5ec77adc6b5
Kyle Yancey <kyancey@outlook.com>
2021-06-17 12:56:05 -0500

    Add project files.

---
+++ Image Padder/Image Padder.Android/MainActivity.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using AndroidX.Core.Content;
+using AndroidX.Core.App;
+using System.Linq;
+using Android;
+using Xamarin.Forms;
+using Android.Database;
+using Android.Provider;
+
+namespace Image_Padder.Droid
+{
+    [Activity(Label = "Image Padder",
+              Icon = "@drawable/icon",
+              Theme = "@style/MainTheme",
+              MainLauncher = true,
+              HardwareAccelerated = true,
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [IntentFilter(new[] { Android.Content.Intent.ActionSend },
+                  DataMimeType = "image/*",
+                  Categories = new[] { Android.Content.Intent.CategoryDefault })]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            checkPermissions();
+
+            string filepath = null;
+            ImageSource imageSource = null;
+            SixLabors.ImageSharp.Image sharpImage = null;
+
+            if (Intent.ClipData != null)
+            {
+                // Get the clip item
+                var clipItem = Intent.ClipData.GetItemAt(0);
+
+                // Just in case we need it later, get the text of the image uri.
+                var imageURI = clipItem.Uri;
+
+                // Get the original file name
+                ICursor returnCursor = ContentResolver.Query(imageURI, null, null, null, null);
+                int nameIndex = returnCursor.GetColumnIndex(IOpenableColumns.DisplayName);
+                returnCursor.MoveToFirst();
+                filepath = returnCursor.GetString(nameIndex);
+
+                // Open our input stream and create an image source from it.
+                var inputStream = ContentResolver.OpenInputStream(imageURI);
+                imageSource = ImageSource.FromStream(() => inputStream);
+
+                // Initialize sharedImage now that we know the byte length of the image
+                byte[] imageByteArray = new byte[inputStream.Length];
+
+                // Read in the bytes of the image and reset the stream for later usage
+                inputStream.Read(imageByteArray, 0, imageByteArray.Length);
+                inputStream.Seek(0, System.IO.SeekOrigin.Begin);
+
+                // Convert byte array to ImageSharp Image format
+                sharpImage = SixLabors.ImageSharp.Image.Load(imageByteArray);
+            }
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            App app = new App();
+            LoadApplication(app);
+
+            // Get the main page instance and send the information to it.
+            MainPage mainPage = (MainPage)app.MainPage;
+            mainPage.setImage(filepath, imageSource, sharpImage);
+        }
+        private void checkPermissions()
+        {
+            String[] requiredPermissions = new String[] { Manifest.Permission.WriteExternalStorage,
+                                                     Manifest.Permission.ReadExternalStorage,
+                                                     Manifest.Permission.AccessMediaLocation};
+
+            foreach (var requiredPermission in requiredPermissions)
+            {
+                if (ContextCompat.CheckSelfPermission(this, requiredPermission) == (int)Permission.Granted)
+                {
+                    // Remove from list of ones to request
+                    requiredPermissions = requiredPermissions.Where(value => value != requiredPermission).ToArray();
+                }
+            }
+
+            if (requiredPermissions.Length != 0)
+            {
+                ActivityCompat.RequestPermissions(this, requiredPermissions, 1);
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Image Padder/Image Padder/MainPage.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Xamarin.Forms;
+
+using AndroidX.Core.Content;
+using Android;
+using AndroidX.Core.App;
+using System.Linq;
+using Android.Database;
+using Android.Provider;
+using Android.Content;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using System.Collections.Generic;
+
+namespace Image_Padder
+{
+    public partial class MainPage : ContentPage
+    {
+        private string filepath;
+        private ImageSource imageSource;
+        private SixLabors.ImageSharp.Image sharpImage;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void setImage(string filepath, ImageSource imageSource, SixLabors.ImageSharp.Image sharpImage)
+        {
+            // If the app is opened from the launcher, don't do anything.
+            if (sharpImage == null)
+            {
+                imageBuffer.Text = "No image found. You must share the image to this app through the share sheet.";
+                shareButton.IsVisible = false;
+                widthEntry.IsVisible = false;
+                heightEntry.IsVisible = false;
+                padButton.IsVisible = false;
+                return;
+            }
+
+            // Store values in class
+            this.filepath = filepath;
+            this.imageSource = imageSource;
+            this.sharpImage = sharpImage;
+
+            filepath = buildFilePath(filepath);
+
+            // We need to save a copy of the file so we can pass it on to the next app.
+            sharpImage.Save(filepath);
+
+            imageBuffer.IsVisible = false;
+            sharedImage.Source = ImageSource.FromFile(filepath);
+            this.filepath = filepath;
+        }
+
+        private string buildFilePath(string filepath)
+        {
+            // Build up the file name.
+            Android.Content.Context context = Android.App.Application.Context;
+            Java.IO.File imagePath = new Java.IO.File(context.FilesDir, "images");
+            imagePath.Mkdirs();
+            filepath = "twitter_decropper" + "." + filepath.Split('.').Last();
+            Java.IO.File newFile = new Java.IO.File(imagePath, filepath);
+            filepath = newFile.AbsolutePath;
+            return filepath;
+        }
+
+        private void shareButton_Clicked(object sender, EventArgs e)
+        {
+            Java.IO.File file = new Java.IO.File(filepath);
+
+            Android.Net.Uri contentUri = FileProvider.GetUriForFile(Android.App.Application.Context,
+                                                       "org.lakeofburningfire.fileprovider", file);
+
+            var sharingIntent = new Intent();
+
+            sharingIntent.AddFlags(ActivityFlags.GrantReadUriPermission);
+            sharingIntent.AddFlags(ActivityFlags.NewTask);
+
+            sharingIntent.SetAction(Intent.ActionSend);
+            sharingIntent.SetType("image/*");
+            sharingIntent.SetData(contentUri);
+            sharingIntent.PutExtra(Intent.ExtraStream, contentUri);
+            Android.App.Application.Context.StartActivity(sharingIntent);
+        }
+
+        private void padButton_Clicked(object sender, EventArgs e)
+        {
+            // Get data from form
+            double widthRatio;
+            try
+            {
+                widthRatio = double.Parse(widthEntry.Text);
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("There was an error: {0}", Ex.Message);
+                widthRatio = 1;
+                widthEntry.Text = 1.ToString();
+            }
+
+            double heightRatio;
+            try
+            {
+                heightRatio = double.Parse(heightEntry.Text);
+            }
+            catch (Exception Ex)
+            {
+                heightRatio = 1;
+                heightEntry.Text = 1.ToString();
+            }
+
+            // Store possible target ratios
+            int heightFromWidth = (int)Math.Ceiling(sharpImage.Width / widthRatio * heightRatio);
+            int widthFromHeight = (int)Math.Ceiling(sharpImage.Height / heightRatio * widthRatio);
+
+            // Find smallest change
+            int heightDifference = heightFromWidth - sharpImage.Height;
+            int widthDifference = widthFromHeight - sharpImage.Width;
+            List<int> differences = new List<int>{ heightDifference, widthDifference };
+            int minDifference = differences.Where(value => value > 0).Min();
+
+            // Set up minimum width and height
+            int minWidth = sharpImage.Width;
+            int minHeight = sharpImage.Height;
+
+            if (minDifference == heightDifference)
+            {
+                minHeight = heightFromWidth;
+            }
+            else if (minDifference == widthDifference)
+            {
+                minWidth = widthFromHeight;
+            }
+
+            //Pad it out to the smallest ratio
+            var cloneImage = sharpImage.Clone(c => c.Resize(new ResizeOptions
+            {
+                Mode = ResizeMode.BoxPad,
+                Position = AnchorPositionMode.Center,
+                Size = new SixLabors.ImageSharp.Size(minWidth, minHeight)
+            }
+                ));
+
+            filepath = buildFilePath(filepath);
+
+            // We need to save a copy of the file so we can pass it on to the next app.
+            cloneImage.Save(filepath);
+            sharedImage.Source = ImageSource.FromFile(filepath);
+        }
+    }
+}
